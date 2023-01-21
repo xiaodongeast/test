7948412f8cd3fdf576600c8d708eca04dbde07da
bugkiller2021 <bugkiller2021@gmail.com>
2023-01-21 15:04:13 -0600

    json load

---
+++ src/ioutil.py
@@ -0,0 +1,20 @@
+import json
+import pickle
+
+
+
+def read_json(in_path):
+    with open(in_path, 'r') as infile:
+        return json.load(infile)
+
+
+def save_json():
+    pass
+
+def read_pickle():
+    pass
+
+def save_pick():
+    pass
+
+
